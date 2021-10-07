15cd444de23328f32aeeb273050b0406dbf6c358
Thilagavathy M <89313155+ThilagavathyManickam@users.noreply.github.com>
2021-10-07 16:16:56 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,31 @@
+//  passing var
+var a=5;
+console.log(a);
+// passing char
+var ch=4;
+console.log(ch);
+// passing function
+function myfunction()
+{
+    return(5*9);
+}
+console.log(myfunction());
+// passing string and variable
+var a=6;
+console.log("the value is:"+a);
+// operators
+let x=6;
+let y=5;
+console.log('x+y=',x+y);
+console.log('x+y=',x-y);
+console.log('x+y=',x/y);
+console.log('x+y=',x*y);
+
+const c=3,d=9;
+console.log(c>d);
+
+console.log(3==3);
+console.log(9==3);
+
+
+
